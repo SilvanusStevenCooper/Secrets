12471f9de25b6ae95929a6f7a6588f41bcb05244
steve <coopersilvanus27@gmail.com>
2023-04-18 00:19:01 +0000

    authentication using bcrypt

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-// md5 encryption
-const md5 = require("md5");
+// requiring bcrypt and setting salt count
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
-// mongoose encryption
-// const encrypt = require("mongoose-encryption");
 
 const app = express();
 app.set("view engine", "ejs");
@@ -23,8 +22,7 @@ const userSchema = new mongoose.Schema({
 });
 
 
-// using mongoose encryption on  schema type and setting it to a specific field
-// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+
 
 const User = mongoose.model("User", userSchema);
 
@@ -41,37 +39,48 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-    const user = new User({
-        email: req.body.userEmail,
-        //md5 encrypted
-        password: md5(req.body.password)
+    // using bcrypt to salt and hash a password
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const user = new User({
+            email: req.body.userEmail,
+            password: hash
+        });
+        user.save()
+        .then(() => {
+            res.render("secrets");
+        })
+        .catch(err => {
+            console.log(err)
+        })
     });
-    user.save()
-    .then(user => {
-        res.render("secrets");
-    })
-    .catch(err => {
-        console.log(err)
-    })
+    
 });
 
-app.post("/login", function(req, res){
+app.post("/login", function(req, res) {
     const userEmail = req.body.userEmail;
-    // md5 encrypted
-    const password = md5(req.body.password);
-
-    User.findOne({email: userEmail})
-    .then(user => {
-        if (user.password === password){
-            res.render("secrets");
-        }else{
-            res.send("Account does not exist. Create an account to login")
+    const password = req.body.password;
+  
+    User.findOne({ email: userEmail })
+      .then(user => {
+        if (user) {
+            // using bcrypt to compare passwords and login a user
+          bcrypt.compare(password, user.password, function(err, result) {
+            if (result === true) {
+              res.render("secrets");
+            } else {
+              res.send("Incorrect password");
+            }
+          });
+        } else {
+          res.send("Account does not exist. Create an account to login");
         }
-    })
-    .catch(err => {
+      })
+      .catch(err => {
         console.log(err);
-    })
-})
+        res.send("An error occurred");
+      });
+  });
+  
 
 
 
